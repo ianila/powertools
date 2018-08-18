a4f76d22cdb66902f1ddeb3fc34fc1c4378b8eb5
Anila <anila@localhost.localdomain>
2018-08-18 21:06:40 +0530

    web: customer signup styles

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,3 +9,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'birthdate', 'password1', 'password2', )
+
+        widgets = {
+            'username': TextInput(attrs={'class': 'w3-input w3-border', 'autofocus': 'True', 'required': True}),
+            'birthdate': TextInput(attrs={'class': 'w3-input w3-border', 'required': True}),
+            'password1': TextInput(attrs={'class': 'w3-input w3-border', 'required': True}),
+            'password2': TextInput(attrs={'class': 'w3-input w3-border', 'required': True}),
+        }
